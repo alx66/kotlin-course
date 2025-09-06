62c22da1478cea23ac95e26ad9a415fcf672e2eb
alx66 <ru.aleks@yandex.ru>
2025-09-06 17:47:39 +0400

    CreateNewBranch

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-   val test1 = "Тестовый коммит"
+   val test1 = "Это новая ветка"
     println(test1)
 }
