28c8b242315bef32133e6d56f515a2e924c797a6
alx66 <ru.aleks@yandex.ru>
2025-09-06 17:43:28 +0400

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+   val test1 = "Тестовый коммит"
+    println(test1)
+}
